cea0c60f388a720105f943de01c6abe2712b59a8
Сафонтьев Иван <safontev@bk.ru>
2026-09-05 21:17:27 +0500

    love

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,18 @@
 <script setup lang="ts">
-import { ref, onBeforeUnmount } from 'vue';
+import { ref } from 'vue';
 
-const x = ref(window.innerWidth - 680);
-const y = ref(window.innerHeight - 352);
-const BUTTON_WIDTH = 140;
-const BUTTON_HEIGHT = 56;
+const x = ref(56);
+const y = ref(50);
 
 const moveRandom = () => {
-  const maxX = window.innerWidth - BUTTON_WIDTH;
-  const maxY = window.innerHeight - BUTTON_HEIGHT;
-  x.value = Math.random() * maxX + BUTTON_WIDTH / 2;
-  y.value = Math.random() * maxY + BUTTON_HEIGHT / 2;
+  const maxX = 100 - 10;
+  const maxY = 100 - 10;
+  x.value = 10 + Math.random() * (maxX - 10);
+  y.value = 10 + Math.random() * (maxY - 10);
 };
 
 const YES = ref<boolean>(false);
+const visible = ref<boolean>(true);
 let heartInterval: ReturnType<typeof setInterval> | null = null;
 
 const createHeart = () => {
@@ -49,34 +48,60 @@ const handleYES = () => {
   }
 };
 
-onBeforeUnmount(() => {
-  if (heartInterval) clearInterval(heartInterval);
-  loveAudio.pause();
-  loveAudio.currentTime = 0;
-});
-
 window.addEventListener('resize', moveRandom);
+
+const words = ['Киса', 'прости меня, пожалуйста', 'я действительно сожалею', 'я тебя очень сильно люблю', 'очень люблю', 'очень приочень люблю', 'безумно люблю', 'тебя люблю',]
+
+function* generator() {
+  for (const word of words) {
+    yield word
+  }
+}
+
+const myGenerator = generator();
+
+var buttonText = ref<string>('Нажми меня');
+
+const handleDear = () => {
+  const next = myGenerator.next();
+  if (!next.done) {
+    buttonText.value = next.value;
+  }
+  if (next.done) {
+    visible.value = false;
+  }
+}
+
 </script>
 
 <template>
-  <span class="absolute top-1/3 left-[25%] text-6xl font-mono font-bold z-2">Прости меня бусинка моя</span>
-  <div class="relative pattern-bg flex items-center justify-center">
+  <div v-if="visible"
+    class="absolute flex items-center justify-center h-screen w-screen text-4xl z-40 bg-rose-600 font-mono">
     <button
-      class="border-2 mr-32 rounded-md cursor-pointer px-8 py-2 text-4xl font-semibold font-mono bg-pink-600 focus:animate-ping"
+      class="border-2 rounded-md cursor-pointer px-8 py-2 text-4xl font-semibold font-mono bg-rose-500 transition-all duration-400 hover:scale-106 hover:bg-rose-400 drop-shadow-md drop-shadow-rose-200"
+      @click="handleDear">
+      {{ buttonText }}
+    </button>
+  </div>
+  <div class="absolute top-1/3 text-4xl font-mono font-bold z-2 w-full lg:text-6xl text-center">Прости меня бусинка моя
+  </div>
+  <div v-if="!visible" class="relative pattern-bg flex items-center justify-center">
+    <button
+      class="border-2 lg:mr-32 mr-42 rounded-md cursor-pointer px-8 py-2 text-4xl font-semibold font-mono bg-pink-600 focus:animate-ping transition-all duration-400 hover:scale-106 hover:bg-pink-500 drop-shadow-md drop-shadow-pink-500"
       @click="handleYES">
       ДА
     </button>
     <button
-      class="absolute border-2 rounded-md cursor-pointer px-8 py-2 text-4xl font-semibold font-mono bg-pink-600 z-20"
+      class="absolute ml-10 lg:ml-0 border-2 rounded-md cursor-pointer px-8 py-2 text-4xl font-semibold font-mono bg-pink-600 z-20 transition-all duration-100 hover:scale-106 hover:bg-pink-500 drop-shadow-md drop-shadow-pink-500"
       :style="{
-        left: x + 'px',
-        top: y + 'px',
+        left: x + '%',
+        top: y + '%',
         transform: 'translate(-50%, -50%)',
       }" @click="moveRandom" @mouseenter="moveRandom">
       НЕТ
     </button>
     <div v-if="YES"
-      class="absolute flex items-center justify-center h-screen w-screen text-4xl z-40 bg-pink-600 font-mono">
+      class="italic absolute flex items-center justify-center h-screen w-screen text-4xl z-40 bg-pink-600 font-mono">
       Я у тебя дурачок, но этот дурачок безумно тебя любит
     </div>
   </div>
